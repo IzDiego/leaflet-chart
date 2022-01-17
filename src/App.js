5bb78f92f4bbf3b422fa8a364fa932a3d31e77a2
Diego Isaac Herrera Ledezma <dhlisaac@gmail.com>
2022-01-17 12:04:33 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./App.css";
+import { dataContribuyentes } from "./components/maps/data";
+import { MyResponsiveLine } from "./components/chart/chart";
+import { chartData } from "./components/chart/chartData";
+import { violetIcon } from "./components/maps/Icons";
 
+const center = [19.432670058417447, -99.13311169359423];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MapContainer
+        center={center}
+        zoom={10}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <TileLayer
+          url="https://api.maptiler.com/maps/osm-standard/{z}/{x}/{y}.jpg?key=dTFewUiAMuFIZwgDv3nB"
+          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        />
+        {dataContribuyentes.features.map((contribuyente) => {
+          const lat = contribuyente.geometry.coordinates[1];
+          const long = contribuyente.geometry.coordinates[0];
+          const coordenada = [lat, long];
+          return (
+            <Marker position={coordenada} icon={violetIcon}>
+              <Popup minWidth='auto'>
+                <div className='chart'>
+                  <MyResponsiveLine data={chartData} />
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </>
   );
 }
 
---
+++ src/components/chart/chartData.js
@@ -0,0 +1,272 @@
+export const chartData = [
+    {
+      "id": "japan",
+      "color": "hsl(135, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 29
+        },
+        {
+          "x": "helicopter",
+          "y": 249
+        },
+        {
+          "x": "boat",
+          "y": 228
+        },
+        {
+          "x": "train",
+          "y": 167
+        },
+        {
+          "x": "subway",
+          "y": 213
+        },
+        {
+          "x": "bus",
+          "y": 281
+        },
+        {
+          "x": "car",
+          "y": 284
+        },
+        {
+          "x": "moto",
+          "y": 74
+        },
+        {
+          "x": "bicycle",
+          "y": 56
+        },
+        {
+          "x": "horse",
+          "y": 68
+        },
+        {
+          "x": "skateboard",
+          "y": 168
+        },
+        {
+          "x": "others",
+          "y": 89
+        }
+      ]
+    },
+    {
+      "id": "france",
+      "color": "hsl(164, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 38
+        },
+        {
+          "x": "helicopter",
+          "y": 200
+        },
+        {
+          "x": "boat",
+          "y": 299
+        },
+        {
+          "x": "train",
+          "y": 183
+        },
+        {
+          "x": "subway",
+          "y": 211
+        },
+        {
+          "x": "bus",
+          "y": 293
+        },
+        {
+          "x": "car",
+          "y": 160
+        },
+        {
+          "x": "moto",
+          "y": 241
+        },
+        {
+          "x": "bicycle",
+          "y": 101
+        },
+        {
+          "x": "horse",
+          "y": 32
+        },
+        {
+          "x": "skateboard",
+          "y": 58
+        },
+        {
+          "x": "others",
+          "y": 135
+        }
+      ]
+    },
+    {
+      "id": "us",
+      "color": "hsl(307, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 289
+        },
+        {
+          "x": "helicopter",
+          "y": 263
+        },
+        {
+          "x": "boat",
+          "y": 104
+        },
+        {
+          "x": "train",
+          "y": 185
+        },
+        {
+          "x": "subway",
+          "y": 200
+        },
+        {
+          "x": "bus",
+          "y": 88
+        },
+        {
+          "x": "car",
+          "y": 22
+        },
+        {
+          "x": "moto",
+          "y": 289
+        },
+        {
+          "x": "bicycle",
+          "y": 195
+        },
+        {
+          "x": "horse",
+          "y": 162
+        },
+        {
+          "x": "skateboard",
+          "y": 87
+        },
+        {
+          "x": "others",
+          "y": 216
+        }
+      ]
+    },
+    {
+      "id": "germany",
+      "color": "hsl(102, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 14
+        },
+        {
+          "x": "helicopter",
+          "y": 262
+        },
+        {
+          "x": "boat",
+          "y": 295
+        },
+        {
+          "x": "train",
+          "y": 109
+        },
+        {
+          "x": "subway",
+          "y": 208
+        },
+        {
+          "x": "bus",
+          "y": 198
+        },
+        {
+          "x": "car",
+          "y": 274
+        },
+        {
+          "x": "moto",
+          "y": 24
+        },
+        {
+          "x": "bicycle",
+          "y": 91
+        },
+        {
+          "x": "horse",
+          "y": 135
+        },
+        {
+          "x": "skateboard",
+          "y": 17
+        },
+        {
+          "x": "others",
+          "y": 220
+        }
+      ]
+    },
+    {
+      "id": "norway",
+      "color": "hsl(306, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 116
+        },
+        {
+          "x": "helicopter",
+          "y": 101
+        },
+        {
+          "x": "boat",
+          "y": 254
+        },
+        {
+          "x": "train",
+          "y": 45
+        },
+        {
+          "x": "subway",
+          "y": 11
+        },
+        {
+          "x": "bus",
+          "y": 246
+        },
+        {
+          "x": "car",
+          "y": 269
+        },
+        {
+          "x": "moto",
+          "y": 97
+        },
+        {
+          "x": "bicycle",
+          "y": 92
+        },
+        {
+          "x": "horse",
+          "y": 76
+        },
+        {
+          "x": "skateboard",
+          "y": 3
+        },
+        {
+          "x": "others",
+          "y": 179
+        }
+      ]
+    }
+  ]
